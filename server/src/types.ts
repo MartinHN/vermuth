75c4b7a4ee86a2d19d5b8e97896d0c4e2a7ddabc
Tintamar <martin.hermant@gmail.com>
2019-06-01 15:23:22 +0200

    WIP

--- server/src/types.ts
+++ server/src/types.ts
@@ -16,20 +16,20 @@
 //     };
 // }
 
-export function getter(n:string,v:any,callback:Function | undefined){
-  let vf = v;
-  if(typeof v!=='function'){
-    vf = ()=>{return v}
-  }
-  socket.on('GET_'+n,()=>{
-    const v = vf();
-    socket.emit('SET_'+n,v)
-    if(typeof callback ==='function'){
-      callback(v)
-    }
-  })
-}
+// export function getter(n:string,v:any,callback:Function | undefined){
+//   let vf = v;
+//   if(typeof v!=='function'){
+//     vf = ()=>{return v}
+//   }
+//   socket.on('GET_'+n,()=>{
+//     const v = vf();
+//     socket.emit('SET_'+n,v)
+//     if(typeof callback ==='function'){
+//       callback(v)
+//     }
+//   })
+// }
 
-export function setter(n:string,vf:function){
-  socket.on('SET_'+n,vf);
-}
+// export function setter(n:string,vf:function){
+//   socket.on('SET_'+n,vf);
+// }
