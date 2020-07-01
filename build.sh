8b3200164220fe16656b44f71ec061a50e179af6
Martin Hermant <martin.hermant@gmail.com>
2020-07-01 18:19:08 +0200

    WIP

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ GEN_ONCE=1 node gen.js
 cd ../viewer
 
 #normal build
-PKG_APP=1 npm run build
+PKG_APP=1 npm run buildFast
 # # Raspberry3 build
 # npx --max_old_space_size=700 vue-cli-service build
 
 cd ../server
 
-npm run buildFast
+npm run build
 
 
