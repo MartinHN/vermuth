1941f780a91a5333ccc32723bbfa385a3e834f40
Martin Hermant <martin.hermant@gmail.com>
2020-07-01 17:01:43 +0200

    add update

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# !!!!!!!!! this script will KILL UI  on rpis !!!!!!!!!!!!!
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
+echo "GIT PULL"
+git pull
+#git reset --soft HEAD
+killall lxpanel # Build process need Ram to avoid heap out of memory errors on poor Pi3s
+echo "BUILD "
+./build.sh
+echo "UPDATE IS DONE"
+
