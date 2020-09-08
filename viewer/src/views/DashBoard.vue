563adf43a62fce14ce31ccbc2391bfbdda571511
tinmar <martin.hermant@gmail.com>
2020-09-08 17:24:04 +0200

    WIP

--- viewer/src/views/DashBoard.vue
+++ viewer/src/views/DashBoard.vue
@@ -1,39 +1,51 @@
 <template>
-  <div class="home">    
-  <v-row>
-  <v-col cols=6>
-    <StateComponent  />
-  </v-col>
-  <v-col>
-    <ChannelRack v-if="allFixture" :displayableFixtureList="allFixture"  />
-  </v-col>
-  </v-row>
-  </div>
+<div class="home">
+    <v-row>
+        <v-col cols="6">
+            <StateComponent />
+        </v-col>
+        <v-col>
+            <!--  <ChannelRack v-if="allFixture" :displayableFixtureList="allFixture"  /> -->
+        </v-col>
+    </v-row>
+</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { State, Action, Getter , Mutation , namespace} from 'vuex-class';
-import ChannelRack from '@/components/ChannelRack.vue'; // @ is an alias to /src
-import StateComponent from  '@/components/StateComponent.vue';
-import { addProp } from "@API/MemoryUtils"
-import UniversesMethods from '../store/universes';
-const universesModule = namespace('universes');
+import {
+    Component,
+    Vue
+} from "vue-property-decorator";
+import {
+    State,
+    Action,
+    Getter,
+    Mutation,
+    namespace
+} from "vuex-class";
+
+import ChannelRack from "@/components/ChannelRack.vue"; // @ is an alias to /src
+import StateComponent from "@/components/StateComponent.vue";
+import {
+    addProp
+} from "@API/MemoryUtils";
+
+import UniversesMethods from "../store/universes";
+const universesModule = namespace("universes");
+
 @Component({
-  components: {
-    ChannelRack,
-    StateComponent,
-  },
+    components: {
+        ChannelRack,
+        StateComponent,
+    },
 })
 export default class HDashboardome extends Vue {
+    @universesModule.State("universe")
+    private universe!: UniversesMethods["universe"];
 
-@universesModule.State('universe') private universe!: UniversesMethods['universe'];
-
-  get allFixture() {
-    return this.universe.fixtureAndGroupList;
-    // return this.universe.sortedFixtureList;
-  }
-
-
+    get allFixture() {
+        return this.universe.fixtureAndGroupList;
+        // return this.universe.sortedFixtureList;
+    }
 }
 </script>
