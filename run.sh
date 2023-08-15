e41ebf2a42599e84da011537fa325301e5449454
tinmar <martin.hermant@gmail.com>
2023-08-15 15:40:23 +0200

    force pnpm if available

--- run.sh
+++ run.sh
@@ -4,5 +4,11 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $DIR/server
 
+RCF="/home/pi/.bash_profile"
+if [[ -f $RCF ]]; then
+    echo "init pi"
+    source $RCF
+fi
+
 #npm run run
-NODE_ENV=production /home/pi/.local/share/pnpm/node distPacked/mainServer.js # not ready yet....
+NODE_ENV=production node distPacked/mainServer.js # not ready yet....
