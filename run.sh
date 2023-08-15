d98b198bbb4c20c846b38e62a68c70e30bc41fc2
tinmar <martin.hermant@gmail.com>
2023-08-15 14:52:08 +0200

    run use pnpm

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# this script may have to be run with sudo
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $DIR/server
 
 #npm run run
-NODE_ENV=production node distPacked/mainServer.js # not ready yet....
-
+NODE_ENV=production /home/pi/.local/share/pnpm/node distPacked/mainServer.js # not ready yet....
